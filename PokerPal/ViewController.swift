42d3e61ef80cb0a7ecf704d7f304ff000c69e6f5
Eric Tyrrell <cicero222@erics-macbook-air.local>
2018-11-13 12:00:50 -0500

    Initial Commit

---
+++ PokerPal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokerPal
+//
+//  Created by Eric Tyrrell on 11/13/18.
+//  Copyright © 2018 Eric Tyrrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
